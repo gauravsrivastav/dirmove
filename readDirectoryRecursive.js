0ce628dea706331ff847982a543f10e867e80721
Gaurav <prince.g.k12@gmail.com>
2019-05-10 18:55:48 +0530

    Update readDirectoryRecursive.js

--- readDirectoryRecursive.js
+++ readDirectoryRecursive.js
@@ -11,7 +11,7 @@ const Promise = require('bluebird');
 function copyFolder(dir, number) {
   try {
     return new Promise((resolve, reject) => {
-      exec('cp -r /Users/omprakash/Desktop/'+number+'/ /Users/omprakash/Desktop/6/'+number+'' , (err, stdout, stderr) => {
+      exec('cp -r /Users/gaurav/Desktop/'+number+'/ /Users/gaurav/Desktop/6/'+number+'' , (err, stdout, stderr) => {
         if (err) {
           reject(err);
           return;
@@ -39,7 +39,7 @@ function getFilesFromDir(dir, number) {
 return new Promise((resolve, reject) => {
   var x = [1, 2, 3, 4, 5];
   x.forEach(function(number) {
-    getFilesFromDir("/Users/omprakash/Desktop/"+number.toString(), number).then(function(data) {
+    getFilesFromDir("/Users/gaurav/Desktop/"+number.toString(), number).then(function(data) {
       resolve(data);
     })
   })
